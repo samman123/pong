7849f5693b4a622be0778bc916a89299ed4747b6
Sam Mathis <smmathis97@gmail.com>
2023-02-02 23:24:56 -0700

    started window

--- pong.py
+++ pong.py
@@ -1,1 +1,12 @@
+import pygame
 
+WIDTH, HEIGHT = 1500, 1000
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("PONG: The Game of the Gods")
+
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
