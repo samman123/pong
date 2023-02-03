f878f7fbbc981dfbae9a23bd1d64080183c389bc
Sam Mathis <smmathis97@gmail.com>
2023-02-03 00:08:19 -0700

    finished window

--- pong.py
+++ pong.py
@@ -10,3 +10,9 @@ def main():
 
     while run:
         for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+    pygame.quit()
+
